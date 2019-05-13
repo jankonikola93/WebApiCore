5b1eca81eb030173c28e77d9db5fd141e5eab654
Nikola Jankovic <nikola.jankovic@addiko.com>
2019-05-13 12:30:37 +0200

    added-LogService

---
+++ Infrastructure/Logger/LogService.cs
@@ -0,0 +1,40 @@
+﻿using ApplicationCore.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Infrastructure.Logger
+{
+    public class LogService : ILogService
+    {
+        private readonly IConfiguration _configuration;
+        public LogService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void WriteLog(string message, string logFileName)
+        {
+            var LogDestination = _configuration.GetValue<string>("AppSetings:LogDestination");
+            StreamWriter SW;
+            if (Directory.Exists(LogDestination))
+            {
+                var destination = System.IO.Path.Combine(LogDestination, logFileName + DateTime.Now.ToString("yyyyMMdd") + ".txt");
+                if (!File.Exists(destination))
+                {
+                    SW = File.CreateText(destination);
+                    SW.Close();
+                }
+
+                using (SW = File.AppendText(destination))
+                {
+                    SW.Write("\r\n\n");
+                    SW.WriteLine(DateTime.Now.ToString("dd-MM-yyyy H:mm:ss") + " " + message);
+                    SW.Close();
+                }
+            }
+        }
+    }
+}
