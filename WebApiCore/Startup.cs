9226dcdd75301d5d1814bf0795bee06bc4edfd93
Nikola Jankovic <jankonikola93@gmail.com>
2019-06-15 23:35:02 +0200

    add-swagger

--- WebApiCore/Startup.cs
+++ WebApiCore/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebApiCore
 {
@@ -32,12 +33,18 @@ namespace WebApiCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<MovieDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MovieDbContext")));
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IGenreService, GenreService>();
             services.AddScoped<IMovieService, MovieService>();
             services.AddScoped<ILogService, LogService>();
-            services.AddDbContext<MovieDbContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MovieDbContext")));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Movies API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +59,16 @@ namespace WebApiCore
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Movies API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
